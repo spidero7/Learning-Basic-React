55d68bd0dbce38760116ea37d51d4084123170d7
spidero7 <pajak.bartlomiej97@gmail.com>
2021-12-03 07:04:36 +0100

    Run prettier

---
+++ react-notes.md
@@ -0,0 +1,9 @@
+# React Notes
+
+## npm
+
+Uruchomienie środowiska:
+1. npm init -y
+2. npm install -D prettier
+3. "format": "prettier --write \"src/**/*.{js, jsx}\"" - dodajemy w "script" w package.json
+4. npm run format
--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-const Pet = (props) => { // props we give information from parent component to child component
-    return React.createElement('div', {}, [
-        React.createElement('h2', {}, props.name),
-        React.createElement('h3', {}, props.animal),
-        React.createElement('h3', {}, props.breed),
-    ])
-}
-
-const App = () => { // component
-    return React.createElement(
-        "div",
-        {},
-        [
-        React.createElement("h1", { id: "brand"}, "Adopt Me!"),
-        React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
-        React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
-        React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier" }),
-        ]
-    );
+const Pet = (props) => {
+  // props we give information from parent component to child component
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root')); // using function to render our React application
+const App = () => {
+  // component
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root")); // using function to render our React application
